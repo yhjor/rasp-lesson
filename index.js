934a4cfbd2202ba9eb673813c5ebf6dc903a5229
Bob Lam <lamh32@kennedykidshk.com>
2018-07-07 16:19:23 +0800

    Make the light on the bread board work(test)

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.info('Hello World')
+var onoff = require('onoff')
+
+console.info('Hello from the other side')
+
+var Gpio = onoff.Gpio
+var led = new Gpio(4, out)
+var interval
+
+interval = setInterval(function() {
+  var value = (led.readSync() + 1) % 2
+  led.write(value, function() {
+    console.log('Changed LED to:' + value)
+  })
+}, 2000)
+
+process.on('SIGINT', function() {
+  clearInterval(interval)
+  led.writeSync(0)
+  led.unexport()
+  console.log('Bye, bye!')
+  process.exit()
+})
