9a820b0373ba165c06643a5bc2497df6c29a1541
George Jor <yhjor@me.com>
2018-07-15 20:50:43 +0800

    add the real world example

---
+++ button_blink_3_times.js
@@ -0,0 +1,27 @@
+const onoff = require('onoff')
+
+const Gpio = onoff.Gpio
+
+const led = new Gpio(18, 'out')
+const button = new Gpio(25, 'in', 'rising', { debounceTimeout: 10 })
+
+button.watch((err, value) => {
+  if (err) {
+    throw err
+  }
+
+  // led.writeSync(led.readSync() ^ 1)
+  led.writeSync(1)
+  led.writeSync(0)
+})
+
+process.on('SIGINT', function() {
+  led.writeSync(0)
+  led.unexport()
+
+  button.writeSync(0)
+  button.unexport(0)
+
+  console.log('Bye, bye!')
+  process.exit()
+})
